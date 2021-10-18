1a591299a3d0737a3b7e732bf02bfc059c1b1818
aissyyy <91513350+aissyyy@users.noreply.github.com>
2021-10-18 11:45:53 +0700

    Add files via upload

---
+++ JavaMethod.java
@@ -0,0 +1,18 @@
+public class JavaMethod{
+
+        static void syaratVaksin(int umur) {
+      
+          if (umur < 12) {
+            System.out.println("Anda Tidak Dapat Di Vaksin"); 
+            
+          } else {
+            System.out.println("Anda Dapat Di Vaksin"); 
+          }
+          
+        } 
+        public static void main(String[] args) { 
+          syaratVaksin(11); 
+        }  
+      }
+      
+    
